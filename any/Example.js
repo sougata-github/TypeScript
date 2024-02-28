9685465c743dcd978e49ff50015e2744e2eb5d5f
Sougata <sougatadas9874@gmail.com>
2024-02-28 20:27:26 +0530

    run example.ts in any .

---
+++ any/Example.js
@@ -0,0 +1,12 @@
+var hero1;
+function getHero1() {
+    return "Thor";
+}
+hero1 = getHero1();
+console.log(hero1);
+var hero2;
+function getHero2() {
+    return true;
+}
+hero2 = getHero2();
+console.log(hero2);
