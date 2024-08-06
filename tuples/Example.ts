7594c331fd836fed0148c0392447b893d3b3123f
Sougata <sougatadas9874@gmail.com>
2024-08-06 11:50:01 +0530

    add console log in tuples.

--- tuples/Example.ts
+++ tuples/Example.ts
@@ -9,6 +9,7 @@ user2.push(344);
 
 //a good practice would be to make a tuple "readonly".
 const user3: readonly [string, number] = ["john doe", 22];
+console.log(user3);
 
 //this will now throw an error:
 // user3.push(344);
